cbe01ae10083ed3b72da6b81646ae77cd177c007
sasikumar860801 <sasikumar860801@gmail.com>
2025-01-26 15:49:49 +0530

    formula screener

---
+++ resources/screener formula.php
@@ -0,0 +1,58 @@
+small cap
+(DII Holding > 10) AND (Market Capitalization < 20000)
+
+Mid cap
+(DII Holding > 10) AND (Market Capitalization > 20000) AND
+(Market Capitalization <80000)
+"
+large cap
+
+(DII holding  > 10) AND (Market Capitalization  > 80000) AND (Current price  < 0.82 * High price )
+
+risky
+
+Market Capitalization <20000 AND
+Market Capitalization >1000 AND
+Promoter holding >30 AND
+Return over 3years >40 AND
+Industry PE >Price to Earning AND
+YOY Quarterly sales growth >10 AND
+YOY Quarterly profit growth >10 AND
+DII holding >1 AND
+Intrinsic Value >Current price 
+
+genral
+(DII holding  > 10) AND (Market Capitalization  > 80000) AND (Current price  < 0.8 * High price )
+DII holding >10 AND
+FII holding >6 AND
+Market Capitalization >80000 AND
+Current price >0.8*High price all time AND
+Current price >0.8*From 52w high
+Correction percentage >10
+
+stock pick 
+Marathon Nextgen Realty  47
+J Kumar Infraprojects 38
+
+
+
+
+large cap - above 80k
+mid cap - 30k to 80k 
+small cap - 5k to 30k 
+risky - below 5k
+
+
+Market Capitalization >30000 AND
+Market Capitalization < 80000 AND
+Promoter holding >50 AND
+Return over 3years >30 AND
+Industry PE >Price to Earning AND
+YOY Quarterly sales growth >10 AND
+YOY Quarterly profit growth >10 AND
+DII holding >5 AND
+DII holding <11 AND
+FII holding <11 AND
+FII holding >5 
+
+industry pe 7 out of 10
